c49ec80fd5cc8ae8e44cdbdf5eb9a2c613a73b43
ShaneMit <shanemit@cox.net>
2020-05-04 18:38:58 -0700

    fetch result displays to page

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.getElementById('searchBtn').addEventListener('click', (event) => {
   event.preventDefault()
   console.log('search')
 })
-if (document.getElementById('cityName').value. length>1) {
-  fetch('')
-}
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ab6befe1c9af034685fd382848930939')
+    .then(r => r.json())
+    .then(city => {
+      console.log(city)
+      document.getElementById('weatherDisplay').innerHTML = `
+      <h1>${city.name}</h1>
+      <p>The current weather is ${JSON.stringify(city.weather[0].main)}
+      `
+})
