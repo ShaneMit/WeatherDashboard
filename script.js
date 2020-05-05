ed071582b2620b34fd5cd8a6066083602f94b1d6
ShaneMit <shanemit@cox.net>
2020-05-04 23:45:05 -0700

    User can now search for a city

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let userInput = document.getElementById('cityName')
+
 document.getElementById('searchBtn').addEventListener('click', (event) => {
   event.preventDefault()
-  console.log('search')
-})
-
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ab6befe1c9af034685fd382848930939')
+  let userCity = userInput.value
+  console.log(userCity)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=ab6befe1c9af034685fd382848930939`)
     .then(r => r.json())
     .then(city => {
       console.log(city)
@@ -11,4 +12,11 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ab6befe1c9
       <h1>${city.name}</h1>
       <p>The current weather is ${JSON.stringify(city.weather[0].main)}
       `
+    })
 })
+// Get Element of text box
+
+// Make user input populate in API call
+
+
+
