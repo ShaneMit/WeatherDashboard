f75bd5cda88890fb3ca924613d5cb56cd1f9eb92
ShaneMit <shanemit@cox.net>
2020-05-04 17:29:39 -0700

    started creating fetch request

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('searchBtn').addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('search')
+})
+if (document.getElementById('cityName').value. length>1) {
+  fetch('')
+}
